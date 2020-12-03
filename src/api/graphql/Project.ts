25d76da19de56f8b18e86266cc1ebd2da683c053
ZHANG Mofan <39518094+BASARANOMO@users.noreply.github.com>
2020-12-03 20:58:50 +0100

    Update: add comments

--- src/api/graphql/Project.ts
+++ src/api/graphql/Project.ts
@@ -34,6 +34,10 @@ export const ProjectQuery = extendType({
   },
 })
 
+/*
+* cascade delete not supported
+* first delete referencing row then delete referenced row
+*/
 export const deleteProject = extendType({
   type: 'Mutation',
   definition(t) {
