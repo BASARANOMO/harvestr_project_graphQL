3dcd0e5b1ffd1dca1d18ed5502fd6b63899fadac
ZHANG Mofan <39518094+BASARANOMO@users.noreply.github.com>
2020-12-07 14:13:43 +0100

    Update: activate apollo tracing

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { createContext } from './context'
 
-const server = new ApolloServer({ schema, context: createContext() })
+const server = new ApolloServer({ 
+  schema, context: createContext(),
+  tracing: true,
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
