4e04ad9c51797ad8ae3ebbdf4ebcd102161eb73c
Michele-Alessandro <almi1797@gmail.com>
2021-01-12 14:54:49 +0100

    Add Redis caching to Apollo

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,24 @@ import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { createContext } from './context'
 
+const { RedisCache } = require('apollo-server-cache-redis');
+const responseCachePlugin = require('apollo-server-plugin-response-cache');
+
+const cacheControl = { defaultMaxAge: 3600 };
+
+const cache = new RedisCache({
+  host: '127.0.0.1',
+  port: 6379,
+  // Options are passed through to the Redis client
+})
+
 const server = new ApolloServer({
   schema,
   context: createContext(),
   tracing: true,
+  cache: cache,
+  cacheControl,
+  plugins: [responseCachePlugin({ cache })],
 })
 
 server.listen().then(({ url }) => {
